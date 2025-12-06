29822807daf897fd63db58630affe16b36c8a17e
Robert Burton <robertgb88@gmail.com>
2025-12-06 00:03:24 -0500

    Created basic pages for states, stations and daily temps. Uploaded data for VA and DC for 2023 and 2024

--- weather_logs/admin.py
+++ weather_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import State, Station, DailyTemp
 
 # Register your models here.
+admin.site.register(State)
+admin.site.register(Station)
+admin.site.register(DailyTemp)
